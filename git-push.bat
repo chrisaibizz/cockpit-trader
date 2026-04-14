3751aff5ea3898b6a0790c01313fae31dd1e4343
Chris <chrisaibizz@gmail.com>
2026-04-14 05:05:00 +0200

    Daily Update 14.04.2026

--- git-push.bat
+++ git-push.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d C:\Users\chris\TradingFloor\cockpit-trader
-git add data.json cockpit-briefing.html index.html
+git add data.json cockpit-briefing.html dashboard-legacy.html journal-data.json index.html
 git commit -m "Daily Update %date%"
 git push origin main
